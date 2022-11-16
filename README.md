62e7776ab7d13b6905aaeabbceedcf39ef92cec3
shuii18 <paullibre18@gmail.com>
2022-11-16 18:25:34 +0100

    project finished

---
+++ README.md
@@ -0,0 +1,19 @@
+# CAR RACING SIMULATOR
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+
+## About <a name = "about"></a>
+
+This is a car Racing simulator built using javascript Objects and classes.
+
+## Getting Started <a name = "getting_started"></a>
+
+You can even download the project or visit: to see the final result
+
+## Usage <a name = "usage"></a>
+
+By default there will be three different cars, and each time you press the start button the property velocity of the cars will be randomized between a value from 1 to 8, and they will advance the value of velocity in pixels to the right every 5 miliseconds, once the first car arribest to the end-line a winner will be declared. You can restart the race with new velocity values by pressing again the start button.
---
+++ main.js
@@ -0,0 +1,61 @@
+class Coche {
+  constructor(id, name, gasolina, color, posicion) {
+    this.name = name;
+    this.velocidad = Math.random() * 2;
+    this.gasolina = gasolina;
+
+    this.color = color;
+    this.id = id;
+    this.posicion = posicion;
+  }
+
+  createUiCar() {
+    const createCar = document.createElement("div");
+    document.getElementById("circuito").appendChild(createCar);
+    createCar.className = "coche";
+    createCar.id = this.id;
+    createCar.style.top = Number(this.posicion) * 50 + "px";
+    createCar.innerHTML = `<img src="img/${this.name}.png">`;
+  }
+
+  nuevaVelocidad() {
+    this.velocidad = Math.round(Math.random() * 8 + 1);
+  }
+}
+
+let coche1 = new Coche("coche1", "Lambo", 100, "blue", "0");
+let coche2 = new Coche("coche2", "Ferrari", 20, "pink", "1");
+let coche3 = new Coche("coche3", "Ambulance", 299, "black", "2");
+
+let coches = [coche1, coche2, coche3];
+
+coche1.createUiCar();
+coche2.createUiCar();
+coche3.createUiCar();
+
+let ganadores = [];
+function myMove() {
+  ganadores = [];
+  document.getElementById("winner").innerHTML = ``;
+
+  for (i = 0; i < coches.length; i++) {
+    coches[i].nuevaVelocidad();
+    let id = null;
+    const elem = document.getElementById(coches[i].id);
+    let velocidad = coches[i].velocidad;
+    let nombre = coches[i].name;
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos >= 750) {
+        clearInterval(id);
+        ganadores.push(nombre);
+        document.getElementById("winner").innerHTML = `${ganadores[0]} WINS!`;
+      } else {
+        pos += velocidad;
+        elem.style.left = pos + "px";
+      }
+    }
+  }
+}
